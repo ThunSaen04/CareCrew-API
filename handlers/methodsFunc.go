71b17503bb4f73077d291991d75a36b20e1baa6e
ธัญพิสิษฐ์ แสนเภา <tanpisis.sa@rmuti.ac.th>
2025-09-23 23:52:36 +0700

    เปลี่ยนเวอร์ชันเฉย ๆ

--- handlers/methodsFunc.go
+++ handlers/methodsFunc.go
@@ -24,8 +24,8 @@ type Res struct { //สำหรับ Swagger
 }
 
 var Name = "CareCrew Backend API"
-var Versions = "0.2.4"
-var Last_Update = "09-21-25"
+var Versions = "0.2.5"
+var Last_Update = "09-23-25"
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
